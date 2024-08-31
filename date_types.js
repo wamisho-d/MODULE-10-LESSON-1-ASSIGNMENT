3c5343ac6c0e5ade87fa9b1976580f27e2e439e6
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-08-31 18:12:57 -0400

    Update date_types.js

---
+++ date_types.js
@@ -0,0 +1,17 @@
+// Question 1 task 1: Declare Variables.
+let studentName; // to store the student's name (string)
+let studentAge;  // to store the student's age (number)
+let studentGrade; // to store the student's grade (number)
+let isPresent; // to store the student's attendence status(boolean)
+
+// Question 1 Task 2: Assign sample values
+studentName = "Alice";  // Assign a sample name
+studentAge = 15;  // Assign a sample age
+studentGrade = 10;  // Assign a sample grade
+isPresent = true;  // Assign a sample attendence status
+
+// Question 1 Task 3: Display the student information using console.log() statements.
+console.log("Student Name:", studentName);
+console.log("student Age:", studentAge);
+console.log("student Grade:", studentGrade);
+console.log("Attendence Status:", isPresent? "Present" : "Absent");
